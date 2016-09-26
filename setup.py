260926b9dbc36ee0ab529ac6f922d48a402acf4a
Ares Ou <weou@cisco.com>
2016-09-25 21:26:10 -0700

    init commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='flask-mongo-model',
+    version='0.0.1',
+    description='A module provides basic ORM feature for MongoDB to Flask applications.',
+    author='Ares Ou',
+    author_email='aresowj@gmail.com',
+    package_dir={'': 'mongo_model'},
+    packages=[''],
+    test_suite='',
+    use_2to3=False,
+    convert_2to3_doctests=[''],
+    use_2to3_fixers=[''],
+    use_2to3_exclude_fixers=[''],
+)
+
